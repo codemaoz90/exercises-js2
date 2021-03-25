7fc49b39a97aa87c8d9ab7832502978392fff533
codemaoz90 <codemaoz@gmail.com>
2021-03-25 21:24:27 +0100

    Exercise 9 finished

--- week-1/Homework/exercise-9.js
+++ week-1/Homework/exercise-9.js
@@ -1,11 +1,8 @@
 /*
 E-commerce
-
 You have to create an e-commerce shopping cart
-
 First of all we need 2 arrays, 
 the list of available products and the list of products introduced in the shopping cart.
-
 1. Fill the products list with at least 4 products, what attributes de we have? 
 at least should have:
 var product = {
@@ -15,15 +12,11 @@ var product = {
   stock:
 }
 id is a unique number that identifies each product
-
 3. Create a function addToShoppingCart to add a product to the shopping cart list giving the product id,
 the function will add the product to the selectedProduct list, and add the price to the totalPrice
-
 4. Create the function removeFrom ShoppingCart to remove a product that a client does not like anymore
-
 5. Create the function shop, the function will empty the list and set 0 in the totalPrice of the shopping cart
 In addition will substract 1 in the product stock of bought products
-
 6. If there is not enough stock, the product cannot be added to the shopping cart
 */
 
@@ -41,9 +34,23 @@ var product2 = {
 	price: 9.99,
 	stock: 2,
 };
+var product3 = {
+	id: 3,
+	name: "Blender Aicook3",
+	price: 59.99,
+	stock: 37,
+};
+var product4 = {
+	id: 4,
+	name: "Hisense ULED TV 55U71QF",
+	price: 709.99,
+	stock: 22,
+};
 
 products.push(product1);
 products.push(product2);
+products.push(product3);
+products.push(product4);
 
 var shoppingCart = {
 	totalPrice: 0,
@@ -51,21 +58,28 @@ var shoppingCart = {
 };
 
 function addToShoppingCart(id) {
-	const productAdded = products.filter((product) => product.id === id);
-	//console.log(productAdded);
-	shoppingCart.selectedProducts.push(productAdded);
-	console.log(productAdded);
-	//shoppingCart["totalPrice"] += productAdded[0]["price"];
+	products.forEach(function (producto) {
+		if (producto.id === id) {
+			shoppingCart.selectedProducts.push(producto);
+			shoppingCart.totalPrice += producto.price;
+		}
+	});
 }
 
 function removeFromShoppingCart(id) {
-	const product = shoppingCart.selectedProducts.filter(
-		(product) => product.id !== id
+	let productsNotRemoved = shoppingCart.selectedProducts.filter(
+		(producto) => producto.id !== id
 	);
-	if (!product) return;
+	shoppingCart.selectedProducts = productsNotRemoved;
+	shoppingCart.totalPrice = productsNotRemoved
+		.map((producto) => producto.price)
+		.reduce((acomulador, valorActual) => acomulador + valorActual);
 }
 
-function shop() {}
+function shop() {
+	shoppingCart.selectedProducts = [];
+	shoppingCart.totalPrice = 0;
+}
 
 //results
 addToShoppingCart(1);
