c8c44e700d35facd86ca0b1b1dc7a1f6d6aed57d
codemaoz90 <codemaoz@gmail.com>
2021-04-02 21:40:27 +0200

    Homeword done

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,25 +1,49 @@
-function setAlarm() {}
+let timerInterval = null;
 
+function setAlarm() {
+	const timeRemaining = document.getElementById("timeRemaining");
+	let alarmSet = document.getElementById("alarmSet").value;
+	console.log(alarmSet);
+	const intervalId = setInterval(() => {
+		console.log(this);
+		if (alarmSet === 0) {
+			// añadir funcion de stop
+			playAlarm();
+			document.body.style.background = "#2c3e50";
+			timeRemaining.style.color = "white";
+
+			clearInterval(intervalId);
+		}
+		if (alarmSet < 10) {
+			timeRemaining.textContent = `Time Remaining: 00:0${alarmSet}`;
+		} else {
+			timeRemaining.textContent = `Time Remaining: 00:${alarmSet}`;
+		}
+		alarmSet--;
+	}, 1000);
+	return intervalId;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
+	document.getElementById("set").addEventListener("click", () => {
+		timerInterval = setAlarm();
+	});
 
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-  });
+	document.getElementById("stop").addEventListener("click", () => {
+		pauseAlarm(timeLeft);
+	});
 }
 
 function playAlarm() {
-  audio.play();
+	audio.play();
 }
 
-function pauseAlarm() {
-  audio.pause();
+function pauseAlarm(timeleft) {
+	audio.pause();
+	clearInterval(timerInterval);
 }
 
 window.onload = setup;
--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,1 +1,50 @@
 // Write your code here
+window.onload = function () {
+	let timeInterval = null;
+	const prev = document.getElementById("prev"),
+		next = document.getElementById("next"),
+		sliderImg = document.getElementById("sliderImg"),
+		autoForwardBtn = document.getElementById("autoForward"),
+		autoBackBtn = document.getElementById("autoBack"),
+		stopBtn = document.getElementById("stop");
+	let idImage = 233;
+
+	prev.addEventListener("click", prevImage);
+	next.addEventListener("click", nextImage);
+	stopBtn.addEventListener("click", stop);
+
+	autoForwardBtn.addEventListener("click", () => {
+		timeInterval = autoForward();
+	});
+
+	autoBackBtn.addEventListener("click", () => {
+		timeInterval = autoBack();
+	});
+
+	function nextImage() {
+		idImage++;
+		sliderImg.src = `https://picsum.photos/id/${idImage}/500/400`;
+	}
+	function prevImage() {
+		idImage--;
+		sliderImg.src = `https://picsum.photos/id/${idImage}/500/400`;
+	}
+
+	function stop() {
+		clearInterval(timeInterval);
+	}
+	function autoForward() {
+		const intervalId = setInterval(() => {
+			idImage++;
+			sliderImg.src = `https://picsum.photos/id/${idImage}/500/400`;
+		}, 2000);
+		return intervalId;
+	}
+	function autoBack() {
+		const intervalId = setInterval(() => {
+			idImage--;
+			sliderImg.src = `https://picsum.photos/id/${idImage}/500/400`;
+		}, 2000);
+		return intervalId;
+	}
+};
